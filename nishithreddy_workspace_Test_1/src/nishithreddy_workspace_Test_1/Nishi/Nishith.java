6419f126fa19dd0d2a8635d1d904e9b25ed53906
Nishith <nishithreddy911@gmail.com>
2023-02-18 18:48:58 +0530

    last

---
+++ nishithreddy_workspace_Test_1/src/nishithreddy_workspace_Test_1/Nishi/Nishith.java
@@ -0,0 +1,10 @@
+package nishithreddy_workspace_Test_1.Nishi;
+
+public class Nishith {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("nihishtfc");
+	}
+
+}
